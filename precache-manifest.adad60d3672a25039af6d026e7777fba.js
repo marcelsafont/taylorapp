cf4178310bb6f5f5f69b4872e677e4c58e354c11
marcelsafont <marcel.safont1983@gmail.com>
2022-01-05 11:36:38 +0100

    Updates

---
+++ precache-manifest.adad60d3672a25039af6d026e7777fba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f99d94699daa266907e5258bb5e6021",
+    "url": "https://marcelsafont.github.io/taylorapp/index.html"
+  },
+  {
+    "revision": "43db7a8be5713eb4a5e8",
+    "url": "https://marcelsafont.github.io/taylorapp/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "43db7a8be5713eb4a5e8",
+    "url": "https://marcelsafont.github.io/taylorapp/static/js/2.6fc39b6b.chunk.js"
+  },
+  {
+    "revision": "df2659f20723d9e518ad965c595b69ee",
+    "url": "https://marcelsafont.github.io/taylorapp/static/js/2.6fc39b6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6da94c48bbafa1403e1",
+    "url": "https://marcelsafont.github.io/taylorapp/static/js/main.dbe14b8c.chunk.js"
+  },
+  {
+    "revision": "c1c5d4f8e4269e83eecb",
+    "url": "https://marcelsafont.github.io/taylorapp/static/js/runtime-main.332f52f1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/taylorapp/precache-manifest.46c2d4697facba75af649620fedde54e.js"
+  "https://marcelsafont.github.io/taylorapp/precache-manifest.adad60d3672a25039af6d026e7777fba.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/taylorapp/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://marcelsafont.github.io/taylorapp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
